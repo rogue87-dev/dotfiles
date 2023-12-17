f7666c007e0f082b67fbe1335775a216d6eee85b
rogue87 <arcoix9@gmail.com>
2023-12-17 22:22:45 +0200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,20 @@
 -- Leader Key
 vim.g.mapleader = ''
 
+-- Options
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.timeoutlen =0 
+vim.o.number = true
+vim.o.expandtab = true
+vim.o.insearch = true
+vim.o.smartindent = true
+vim.o.clipboard = "unnamedplus"
+
+-- Key Mappings
+vim.api.nvim_set_keymap('n', 'c', ':Commentary<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-b>', ':Neotree toggle<CR>', { noremap = true, silent = true})
+
 -- Install Packer if not already installed
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 1 then
@@ -33,20 +47,7 @@ require('packer').startup(function()
         })
     }
 
-    use {"ellisonleao/glow.nvim", config = function() require("glow").setup(
-
-	    {
-            glow_path = "", -- will be filled automatically with your glow bin in $PATH, if any
-            install_path = "~/.local/bin", -- default path for installing glow binary
-            border = "shadow", -- floating window border config
-            style = "dark", -- filled automatically with your current editor background, you can override using glow json style
-            pager = false,
-            width = 80,
-            height = 100,
-            width_ratio = 0.7, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
-            height_ratio = 0.7,
-        }
-        ) end}
+    use {"ellisonleao/glow.nvim", config = function() require("glow").setup() end}
 
     use {   --Neotree
         "nvim-neo-tree/neo-tree.nvim",
@@ -118,17 +119,3 @@ require('nvim-treesitter.configs').setup {
 
 --  Other Plugins
 vim.cmd 'colorscheme onedark'
-
--- Options
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.timeoutlen =0 
-vim.o.number = true
-vim.o.expandtab = true
-vim.o.insearch = true
-vim.o.smartindent = true
-vim.o.clipboard = "unnamedplus"
-
--- Key Mappings
-vim.api.nvim_set_keymap('n', 'c', ':Commentary<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-b>', ':Neotree toggle<CR>', { noremap = true, silent = true})

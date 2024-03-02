78c5a500c01f59812d055e6d17fb5e071d94481b
Rogue87 <68822497+rogue87-dev@users.noreply.github.com>
2024-03-02 14:50:50 +0200

    Update options.lua

--- .config/nvim/lua/rogue/core/options.lua
+++ .config/nvim/lua/rogue/core/options.lua
@@ -1,10 +1,14 @@
 -- Options
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+
 vim.o.number = true
 vim.o.relativenumber = true
+
 vim.o.termguicolors = true
-vim.o.expandtab = true
 vim.o.insearch = true
-vim.o.smartindent = true
 vim.o.clipboard = "unnamedplus"
+
+vim.o.scrolloff = 5

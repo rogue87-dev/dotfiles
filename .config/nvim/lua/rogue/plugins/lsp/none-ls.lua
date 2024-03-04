cb3ccc0dfa3ca0c0bd297367238b409900c883fe
Rogue87 <68822497+rogue87-dev@users.noreply.github.com>
2024-03-04 14:39:22 +0200

    Add files via upload

--- .config/nvim/lua/rogue/plugins/lsp/none-ls.lua
+++ .config/nvim/lua/rogue/plugins/lsp/none-ls.lua
@@ -1,1 +1,25 @@
-return {}
+return {
+	"nvimtools/none-ls.nvim",
+	enabled = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			debug = false,
+
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.completion.luasnip,
+
+				-- null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.prettier,
+
+				null_ls.builtins.completion.spell,
+			},
+		})
+	end,
+}

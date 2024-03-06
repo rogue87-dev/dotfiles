3c2d5082918fbef3ccddd920cafa9f00f80aa8eb
rogue <rogue87.dev@gmail.com>
2024-03-06 11:42:28 +0200

    finally. me neovim IDE is complete.

--- .config/nvim/lua/rogue/plugins/autocmp/cmp-core.lua
+++ .config/nvim/lua/rogue/plugins/autocmp/cmp-core.lua
@@ -6,6 +6,7 @@ return {
     "hrsh7th/cmp-nvim-lsp", --source for lsp
     "hrsh7th/cmp-buffer", --source for text in buffer
     "hrsh7th/cmp-path",   --source for file system paths
+    "hrsh7th/cmp-nvim-lsp-signature-help"
   },
   config = function()
     local cmp = require("cmp")
@@ -34,6 +35,7 @@ return {
       }, {
         { name = "path" },
         { name = "buffer" },
+        { name = "nvim_lsp_signature_help" }
       }),
     })
   end,
--- .config/nvim/lua/rogue/plugins/programs/glow.lua
+++ .config/nvim/lua/rogue/plugins/programs/glow.lua
@@ -9,7 +9,7 @@ return {
       glow_path = "/usr/bin/glow", -- will be filled automatically with your glow bin in $PATH, if any
       install_path = "~/.local/bin", -- default path for installing glow binary
       border = "shadow",          -- floating window border config
-      style = "dark|light",       -- filled automatically with your current editor background, you can override using glow json style
+      style = "dark",       -- filled automatically with your current editor background, you can override using glow json style
       pager = false,
       width = 80,
       height = 100,
